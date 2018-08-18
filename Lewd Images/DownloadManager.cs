e1e2475d7070a6ad6cad8351fc86672274b0e500
thejayduck <ardaerda001@hotmail.com>
2018-08-18 22:48:00 +0300

    Initial Commit

---
+++ Lewd Images/DownloadManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+using Java.Net;
+
+namespace Lewd_Images
+{
+    public class DownloadManager : AsyncTask<string, string, string>
+    {
+        private ProgressDialog pDialog;
+        private ImageView imgView;
+        private Context context;
+        private string imageName;
+
+        public DownloadManager(Context context, ImageView imgView, string imageName)
+        {
+            this.context = context;
+            this.imgView = imgView;
+            this.imageName = imageName;
+        }
+
+        protected override void OnPreExecute()
+        {
+            pDialog = new ProgressDialog(context);
+            pDialog.SetMessage("Downloading File Please Wait...");
+            pDialog.Indeterminate = false;
+            pDialog.Max = 100;
+            pDialog.SetProgressStyle(ProgressDialogStyle.Horizontal);
+            pDialog.SetCancelable(true);
+            pDialog.Show();
+            base.OnPreExecute();
+        }
+
+        protected override void OnProgressUpdate(params string[] values)
+        {
+            base.OnProgressUpdate(values);
+            pDialog.SetProgressNumberFormat(values[0]);
+            pDialog.Progress = int.Parse(values[0]);
+        }
+
+        protected override void OnPostExecute(string result)
+        {
+            //string storagePath = Android.OS.Environment.ExternalStorageDirectory.Path;
+            //string filePath = System.IO.Path.Combine(storagePath, "download.jpg");
+
+            //imgView.SetImageDrawable(Drawable.CreateFromPath(filePath));
+            pDialog.Dismiss();
+        }
+
+        protected override string RunInBackground(params string[] @params)
+        {
+            string storagePath = Android.OS.Environment.ExternalStorageDirectory.Path;
+            string filePath = System.IO.Path.Combine(storagePath, $"download{imageName}");
+
+            int count;
+
+            try
+            {
+                URL url = new URL(@params[0]);
+                URLConnection connection = url.OpenConnection();
+                connection.Connect();
+                int LenghtOfFile = connection.ContentLength;
+                InputStream input = new BufferedInputStream(url.OpenStream(), LenghtOfFile);
+                OutputStream output = new FileOutputStream(filePath);
+
+                byte[] data = new byte[1024];
+                long total = 0;
+                while ((count = input.Read(data)) != -1)
+                {
+                    total += count;
+                    PublishProgress("" + (int)((total / 100 / LenghtOfFile)));
+                    output.Write(data, 0, count);
+                }
+                output.Flush();
+                output.Close();
+                input.Close();
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+    }
+}
---
+++ Lewd Images/MainActivity.cs
@@ -0,0 +1,73 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+using System.Net;
+using Android.Webkit;
+using System.IO;
+using System;
+using Android;
+using Android.Support.V4.App;
+using Android.Content.PM;
+using Android.Graphics;
+
+namespace Lewd_Images
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ImageView imagePanel;
+        string imageLink;
+        private static String[] PERMISSIONS_INTERNET = { Manifest.Permission.Internet, Manifest.Permission.WriteExternalStorage };
+        private static int REQUEST_INTERNET = 1;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            //wbView.Settings.BuiltInZoomControls = true; wbView.Settings.DisplayZoomControls = true;
+            imagePanel = FindViewById<ImageView>(Resource.Id.imageView);
+            FindViewById<Button>(Resource.Id.btnGenerate).Click += (o, e) => GetImage();
+            FindViewById<Button>(Resource.Id.btnDownload).Click += delegate
+            {
+                DownloadManager download = new DownloadManager(this, imagePanel, imageLink.Split('"')[3]);
+                download.Execute(imageLink.Split('"')[3]);
+                Toast.MakeText(this, $"Downloading {imageLink.Split('"')[3]}!", ToastLength.Long).Show();
+            };
+            CheckForPermissions();
+        }
+
+        private void CheckForPermissions()
+        {
+            if (ActivityCompat.CheckSelfPermission(this, Manifest.Permission.WriteExternalStorage) != (int)Permission.Granted || ActivityCompat.CheckSelfPermission(this, Manifest.Permission.Internet) != (int)Permission.Granted)
+            {
+                ActivityCompat.RequestPermissions(this, PERMISSIONS_INTERNET, REQUEST_INTERNET);
+            }
+        }
+
+        void GetImage()
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://nekos.life/api/v2/img/lewd");
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                imageLink = reader.ReadToEnd();
+            }
+
+            imagePanel.SetImageBitmap(GetImageBitmapFromUrl(imageLink.Split('"')[3]));
+            Toast.MakeText(this, "Generated New Image", ToastLength.Long).Show();
+        }
+        public Bitmap GetImageBitmapFromUrl(string url)
+        {
+            using (var webClient = new WebClient())
+            {
+                var imageBytes = webClient.DownloadData(url);
+                if (imageBytes != null && imageBytes.Length > 0) return BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+            }
+            return null;
+        }
+    }
+}
