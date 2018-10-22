f4a0b5b6364bbf9372a27208428fa152e780d2c6
TheJayDuck <ardaerda001@hotmail.com>
2018-10-22 21:11:44 +0300

    95% of the Dark Theme is done

--- Lewd Images/Resources/Resource.Designer.cs
+++ Lewd Images/Resources/Resource.Designer.cs
@@ -1235,44 +1235,44 @@ namespace Lewd_Images
 		public partial class Color
 		{
 			
-			// aapt resource value: 0x7f0a0054
-			public const int abc_background_cache_hint_selector_material_dark = 2131361876;
-			
-			// aapt resource value: 0x7f0a0055
-			public const int abc_background_cache_hint_selector_material_light = 2131361877;
-			
 			// aapt resource value: 0x7f0a0056
-			public const int abc_btn_colored_borderless_text_material = 2131361878;
+			public const int abc_background_cache_hint_selector_material_dark = 2131361878;
 			
 			// aapt resource value: 0x7f0a0057
-			public const int abc_btn_colored_text_material = 2131361879;
+			public const int abc_background_cache_hint_selector_material_light = 2131361879;
 			
 			// aapt resource value: 0x7f0a0058
-			public const int abc_color_highlight_material = 2131361880;
+			public const int abc_btn_colored_borderless_text_material = 2131361880;
 			
 			// aapt resource value: 0x7f0a0059
-			public const int abc_hint_foreground_material_dark = 2131361881;
+			public const int abc_btn_colored_text_material = 2131361881;
 			
 			// aapt resource value: 0x7f0a005a
-			public const int abc_hint_foreground_material_light = 2131361882;
+			public const int abc_color_highlight_material = 2131361882;
+			
+			// aapt resource value: 0x7f0a005b
+			public const int abc_hint_foreground_material_dark = 2131361883;
+			
+			// aapt resource value: 0x7f0a005c
+			public const int abc_hint_foreground_material_light = 2131361884;
 			
 			// aapt resource value: 0x7f0a0008
 			public const int abc_input_method_navigation_guard = 2131361800;
 			
-			// aapt resource value: 0x7f0a005b
-			public const int abc_primary_text_disable_only_material_dark = 2131361883;
-			
-			// aapt resource value: 0x7f0a005c
-			public const int abc_primary_text_disable_only_material_light = 2131361884;
-			
 			// aapt resource value: 0x7f0a005d
-			public const int abc_primary_text_material_dark = 2131361885;
+			public const int abc_primary_text_disable_only_material_dark = 2131361885;
 			
 			// aapt resource value: 0x7f0a005e
-			public const int abc_primary_text_material_light = 2131361886;
+			public const int abc_primary_text_disable_only_material_light = 2131361886;
 			
 			// aapt resource value: 0x7f0a005f
-			public const int abc_search_url_text = 2131361887;
+			public const int abc_primary_text_material_dark = 2131361887;
+			
+			// aapt resource value: 0x7f0a0060
+			public const int abc_primary_text_material_light = 2131361888;
+			
+			// aapt resource value: 0x7f0a0061
+			public const int abc_search_url_text = 2131361889;
 			
 			// aapt resource value: 0x7f0a0009
 			public const int abc_search_url_text_normal = 2131361801;
@@ -1283,29 +1283,29 @@ namespace Lewd_Images
 			// aapt resource value: 0x7f0a000b
 			public const int abc_search_url_text_selected = 2131361803;
 			
-			// aapt resource value: 0x7f0a0060
-			public const int abc_secondary_text_material_dark = 2131361888;
-			
-			// aapt resource value: 0x7f0a0061
-			public const int abc_secondary_text_material_light = 2131361889;
-			
 			// aapt resource value: 0x7f0a0062
-			public const int abc_tint_btn_checkable = 2131361890;
+			public const int abc_secondary_text_material_dark = 2131361890;
 			
 			// aapt resource value: 0x7f0a0063
-			public const int abc_tint_default = 2131361891;
+			public const int abc_secondary_text_material_light = 2131361891;
 			
 			// aapt resource value: 0x7f0a0064
-			public const int abc_tint_edittext = 2131361892;
+			public const int abc_tint_btn_checkable = 2131361892;
 			
 			// aapt resource value: 0x7f0a0065
-			public const int abc_tint_seek_thumb = 2131361893;
+			public const int abc_tint_default = 2131361893;
 			
 			// aapt resource value: 0x7f0a0066
-			public const int abc_tint_spinner = 2131361894;
+			public const int abc_tint_edittext = 2131361894;
 			
 			// aapt resource value: 0x7f0a0067
-			public const int abc_tint_switch_track = 2131361895;
+			public const int abc_tint_seek_thumb = 2131361895;
+			
+			// aapt resource value: 0x7f0a0068
+			public const int abc_tint_spinner = 2131361896;
+			
+			// aapt resource value: 0x7f0a0069
+			public const int abc_tint_switch_track = 2131361897;
 			
 			// aapt resource value: 0x7f0a000c
 			public const int accent_material_dark = 2131361804;
@@ -1355,14 +1355,20 @@ namespace Lewd_Images
 			// aapt resource value: 0x7f0a0052
 			public const int colorBackground_Dark = 2131361874;
 			
+			// aapt resource value: 0x7f0a0054
+			public const int colorFab = 2131361876;
+			
 			// aapt resource value: 0x7f0a004f
 			public const int colorPrimary = 2131361871;
 			
 			// aapt resource value: 0x7f0a0050
 			public const int colorPrimaryDark = 2131361872;
 			
-			// aapt resource value: 0x7f0a0068
-			public const int common_google_signin_btn_text_dark = 2131361896;
+			// aapt resource value: 0x7f0a0053
+			public const int colorToolbar = 2131361875;
+			
+			// aapt resource value: 0x7f0a006a
+			public const int common_google_signin_btn_text_dark = 2131361898;
 			
 			// aapt resource value: 0x7f0a0000
 			public const int common_google_signin_btn_text_dark_default = 2131361792;
@@ -1376,8 +1382,8 @@ namespace Lewd_Images
 			// aapt resource value: 0x7f0a0003
 			public const int common_google_signin_btn_text_dark_pressed = 2131361795;
 			
-			// aapt resource value: 0x7f0a0069
-			public const int common_google_signin_btn_text_light = 2131361897;
+			// aapt resource value: 0x7f0a006b
+			public const int common_google_signin_btn_text_light = 2131361899;
 			
 			// aapt resource value: 0x7f0a0004
 			public const int common_google_signin_btn_text_light_default = 2131361796;
@@ -1391,14 +1397,14 @@ namespace Lewd_Images
 			// aapt resource value: 0x7f0a0007
 			public const int common_google_signin_btn_text_light_pressed = 2131361799;
 			
-			// aapt resource value: 0x7f0a006a
-			public const int common_google_signin_btn_tint = 2131361898;
+			// aapt resource value: 0x7f0a006c
+			public const int common_google_signin_btn_tint = 2131361900;
 			
 			// aapt resource value: 0x7f0a0044
 			public const int design_bottom_navigation_shadow_color = 2131361860;
 			
-			// aapt resource value: 0x7f0a006b
-			public const int design_error = 2131361899;
+			// aapt resource value: 0x7f0a006d
+			public const int design_error = 2131361901;
 			
 			// aapt resource value: 0x7f0a0045
 			public const int design_fab_shadow_end_color = 2131361861;
@@ -1424,8 +1430,8 @@ namespace Lewd_Images
 			// aapt resource value: 0x7f0a004c
 			public const int design_snackbar_background_color = 2131361868;
 			
-			// aapt resource value: 0x7f0a006c
-			public const int design_tint_password_toggle = 2131361900;
+			// aapt resource value: 0x7f0a006e
+			public const int design_tint_password_toggle = 2131361902;
 			
 			// aapt resource value: 0x7f0a001a
 			public const int dim_foreground_disabled_material_dark = 2131361818;
@@ -1454,8 +1460,8 @@ namespace Lewd_Images
 			// aapt resource value: 0x7f0a0022
 			public const int highlighted_text_material_light = 2131361826;
 			
-			// aapt resource value: 0x7f0a0053
-			public const int ic_launcher_background = 2131361875;
+			// aapt resource value: 0x7f0a0055
+			public const int ic_launcher_background = 2131361877;
 			
 			// aapt resource value: 0x7f0a0023
 			public const int material_blue_grey_800 = 2131361827;
@@ -1550,11 +1556,11 @@ namespace Lewd_Images
 			// aapt resource value: 0x7f0a003e
 			public const int switch_thumb_disabled_material_light = 2131361854;
 			
-			// aapt resource value: 0x7f0a006d
-			public const int switch_thumb_material_dark = 2131361901;
+			// aapt resource value: 0x7f0a006f
+			public const int switch_thumb_material_dark = 2131361903;
 			
-			// aapt resource value: 0x7f0a006e
-			public const int switch_thumb_material_light = 2131361902;
+			// aapt resource value: 0x7f0a0070
+			public const int switch_thumb_material_light = 2131361904;
 			
 			// aapt resource value: 0x7f0a003f
 			public const int switch_thumb_normal_material_dark = 2131361855;
